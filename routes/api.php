9f966d83c67ad932f6ef7d99508e9bc394c4986d
oiuv <i@oiuv.cn>
2019-05-23 21:14:37 +0800

    修正找回密码邮件BUG

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+/**
+ * IIS 服务器请在【HTTP响应标头】中设置一下值
+ * 非 IIS 服务器请取消一下注释
+ */
 // header('Access-Control-Allow-Origin: *');
 // header('Access-Control-Allow-Headers: Authorization, Content-Type, Access-Control-Allow-Headers, X-Requested-With');
 // header('Access-Control-Allow-Methods: *');
