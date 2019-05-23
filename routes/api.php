cd40bab2b1197c472cd1465d2720004f6d2d9e85
oiuv <i@oiuv.cn>
2019-05-24 01:37:38 +0800

    update for IIS

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
  * IIS 服务器请在【HTTP响应标头】中设置以下值
  * 非 IIS 服务器请取消以下注释
  */
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Authorization, Content-Type, Access-Control-Allow-Headers, X-Requested-With');
-header('Access-Control-Allow-Methods: *');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Headers: Authorization, Content-Type, Access-Control-Allow-Headers, X-Requested-With');
+// header('Access-Control-Allow-Methods: *');
 
 // Auth
 Route::post('auth/register', 'AuthController@register');
